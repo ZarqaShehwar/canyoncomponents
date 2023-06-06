652a5ede7b50b581dfe0567434fe2ff7db1f1bce
Malik Abdullah <76260270+malikabdullahnaazar@users.noreply.github.com>
2023-06-06 10:06:21 +0500

    app chnege

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 
-import React, { useState } from 'react';
+import React from 'react';
+// import  { useState } from 'react';
 import './App.css';
 import { UserContext } from '../src/UserContext'
-import { Routes, Route, Router } from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import RequestQuote from './components/REquestQutoe/RequestQuote';
 function App() {
