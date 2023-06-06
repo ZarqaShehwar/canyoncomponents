d60cf5bc8261273c03f89e5357a029768b324c5d
Malik Abdullah <76260270+malikabdullahnaazar@users.noreply.github.com>
2023-06-06 11:20:02 +0500

    implement more functionnality

--- src/components/REquestQutoe/RequestQuote.jsx
+++ src/components/REquestQutoe/RequestQuote.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import '../REquestQutoe/request.css'
 import { AiOutlineCloseCircle } from 'react-icons/ai'
+import { useNavigate  } from "react-router-dom";
 import Layout from '../Layout/Layout'
 const RequestQuote = () => {
+    const  navigate = useNavigate();
+const handlecancel=()=>{
+    navigate(-1);
+}
     return (
         <>
             <Layout>
 
                 <div className='container '>
                     <div className="icon">
-                        <AiOutlineCloseCircle size={35} />
+                        <AiOutlineCloseCircle size={35} onClick={handlecancel} />
                     </div>
                     <h1 className='head'>REQUEST A QUOTE</h1>
                     <p className='para'>
@@ -18,25 +23,32 @@ const RequestQuote = () => {
                         <strong>Note:</strong> If you call, please reference the <strong>Item #</strong> below for fast service.
                     </p>
                     <form action="" className='form' method="post">
-                        <label htmlFor="item">Item</label>
+                        <label htmlFor="item"><strong>Item</strong></label>
                         <input type="text" name="" id="item" />
-
-                        <div>
-                            <label htmlFor="Name">Name</label>
+                    <div className='flex'>
+                        
+                        <div className='space-betw'>
+                            <label htmlFor="Name"><strong>Name</strong></label>
                             <input type="text" name="" id="Name" />
-                            <label htmlFor="Lname">Last Name</label>
+                            
+                        </div>
+                        <div className='space-betw'>
+                            
+                        <label htmlFor="Lname"><strong>Last Name</strong></label>
                             <input type="text" name="" id="Lname" />
                         </div>
+                    
+                    </div>
 
-                        <label htmlFor="Email">Email</label>
+                        <label htmlFor="Email"><strong>Email</strong></label>
                         <input type="text" name="" id="Email" />
-                        <label htmlFor="Phone">Phone</label>
+                        <label htmlFor="Phone"><strong>Phone</strong></label>
                         <input type="text" name="" id="Phone" />
-                        <label htmlFor="Quantity">Quantity</label>
+                        <label htmlFor="Quantity"><strong>Quantity</strong></label>
                         <input type="text" name="" id="Quantity" />
-                        <label htmlFor="tprice">Target Price</label>
+                        <label htmlFor="tprice"><strong>Target Price</strong></label>
                         <input type="text" name="" id="tprice" />
-                        <label htmlFor="comments">Comments</label>
+                        <label htmlFor="comments"><strong>Comments</strong></label>
                         <textarea name="" id="comments" cols="30" rows="10" />
 
                         <button type="submit" className='subbtn'>Submit</button>
